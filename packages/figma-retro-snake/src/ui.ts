c17d10a66213542c904a90eccc650a6e90ebd8bf
gnawx <stevanus.satria@gmail.com>
2020-02-24 22:15:24 +0800

    basic game done

--- packages/figma-retro-snake/src/ui.ts
+++ packages/figma-retro-snake/src/ui.ts
@@ -30,20 +30,23 @@ enum Direction {
   RIGHT,
 }
 
-const initialSnakeLength: number = 5;
+const initialSnakeLength: number = 20;
 const unitSize: number[] = [10, 10];
 const initialDirection: Direction = Direction.UP;
 const foodPosition: number[] = [0, 0];
 const nextHeadPosition: number[] = [190, 190];
+let nextTailVelocity = [0, 0];
 let lastFrameUpdate: number = 0;
 let snakeLength: number = initialSnakeLength;
 let headToTailVelocity: number[][] = [];
 let currentDirection: Direction = initialDirection;
 let isFoodEaten: boolean = true;
+let isDead: boolean = false;
 
 const initialize = () => {
   for (let i: number = 0; i < snakeLength; i++) {
     headToTailVelocity.push([0, 0]);
+    nextTailVelocity[1] = -1;
   }
 };
 
@@ -59,65 +62,78 @@ const getCanvas2dContext = (id: string) => {
 };
 
 const spawnFood = () => {
-  foodPosition[0] = Math.floor(Math.random() * 390);
-  foodPosition[1] = Math.floor(Math.random() * 390);
+  foodPosition[0] = Math.round(Math.random() * 39) * 10;
+  foodPosition[1] = Math.round(Math.random() * 39) * 10;
+  console.log(foodPosition);
   isFoodEaten = false;
 };
 
 const updateSnake = () => {
-  // pop last tail velocity
-  headToTailVelocity.pop();
-  // update velocity array
-  switch (currentDirection) {
-    case Direction.UP:
-      headToTailVelocity.unshift([0, -1]);
-      break;
-    case Direction.DOWN:
-      headToTailVelocity.unshift([0, 1]);
-      break;
-    case Direction.LEFT:
-      headToTailVelocity.unshift([-1, 0]);
-      break;
-    case Direction.RIGHT:
-      headToTailVelocity.unshift([1, 0]);
-      break;
-    default:
-      break;
+  if (!isDead) {
+    // pop last tail velocity
+    headToTailVelocity.pop();
+    // update velocity array
+    switch (currentDirection) {
+      case Direction.UP:
+        headToTailVelocity.unshift([0, -1]);
+        break;
+      case Direction.DOWN:
+        headToTailVelocity.unshift([0, 1]);
+        break;
+      case Direction.LEFT:
+        headToTailVelocity.unshift([-1, 0]);
+        break;
+      case Direction.RIGHT:
+        headToTailVelocity.unshift([1, 0]);
+        break;
+      default:
+        break;
+    }
+    // get canvas context
+    const context = getCanvas2dContext("snake");
+    // get next head position
+    nextHeadPosition[0] > 390
+      ? (nextHeadPosition[0] = 0)
+      : nextHeadPosition[0] < 0
+      ? (nextHeadPosition[0] = 390)
+      : (nextHeadPosition[0] = nextHeadPosition[0] + unitSize[0] * headToTailVelocity[0][0]);
+    nextHeadPosition[1] > 390
+      ? (nextHeadPosition[1] = 0)
+      : nextHeadPosition[1] < 0
+      ? (nextHeadPosition[1] = 390)
+      : (nextHeadPosition[1] = nextHeadPosition[1] + unitSize[1] * headToTailVelocity[0][1]);
+    // update head position
+    let unitPosition: number[] = [...nextHeadPosition];
+    // clear the canvas
+    context.fillStyle = "#333333";
+    context.fillRect(0, 0, 400, 400);
+    // draw food
+    context.fillStyle = "#FFDDD1";
+    context.fillRect(foodPosition[0], foodPosition[1], unitSize[0], unitSize[1]);
+    console.log(nextHeadPosition);
+    if (nextHeadPosition[0] === foodPosition[0] && nextHeadPosition[1] === foodPosition[1]) {
+      isFoodEaten = true;
+      snakeLength++;
+      headToTailVelocity.push(nextTailVelocity);
+    }
+    // draw snake
+    headToTailVelocity.forEach(unitVelocity => {
+      context.fillStyle = "#D4C2E1";
+      context.fillRect(unitPosition[0], unitPosition[1], unitSize[0], unitSize[1]);
+      unitPosition[0] - unitSize[0] * unitVelocity[0] > 390
+        ? (unitPosition[0] = 0)
+        : unitPosition[0] - unitSize[0] * unitVelocity[0] < 0
+        ? (unitPosition[0] = 400)
+        : (unitPosition[0] -= unitSize[0] * unitVelocity[0]);
+      unitPosition[1] - unitSize[1] * unitVelocity[1] > 390
+        ? (unitPosition[1] = 0)
+        : unitPosition[1] - unitSize[1] * unitVelocity[1] < 0
+        ? (unitPosition[1] = 400)
+        : (unitPosition[1] -= unitSize[1] * unitVelocity[1]);
+      if (unitPosition[0] === nextHeadPosition[0] && unitPosition[1] === nextHeadPosition[1]) {
+        isDead = true;
+      }
+      nextTailVelocity = [...unitVelocity];
+    });
   }
-  // get canvas context
-  const context = getCanvas2dContext("snake");
-  // get next head position
-  nextHeadPosition[0] > 390
-    ? (nextHeadPosition[0] = 0)
-    : nextHeadPosition[0] < 0
-    ? (nextHeadPosition[0] = 390)
-    : (nextHeadPosition[0] = nextHeadPosition[0] + unitSize[0] * headToTailVelocity[0][0]);
-  nextHeadPosition[1] > 390
-    ? (nextHeadPosition[1] = 0)
-    : nextHeadPosition[1] < 0
-    ? (nextHeadPosition[1] = 390)
-    : (nextHeadPosition[1] = nextHeadPosition[1] + unitSize[1] * headToTailVelocity[0][1]);
-  // update head position
-  let unitPosition: number[] = [...nextHeadPosition];
-  // clear the canvas
-  context.fillStyle = "#333333";
-  context.fillRect(0, 0, 400, 400);
-  // draw snake
-  headToTailVelocity.forEach(unitVelocity => {
-    context.fillStyle = "#D4C2E1";
-    context.fillRect(unitPosition[0], unitPosition[1], unitSize[0], unitSize[1]);
-    unitPosition[0] - unitSize[0] * unitVelocity[0] > 390
-      ? (unitPosition[0] = 0)
-      : unitPosition[0] - unitSize[0] * unitVelocity[0] < 0
-      ? (unitPosition[0] = 400)
-      : (unitPosition[0] -= unitSize[0] * unitVelocity[0]);
-    unitPosition[1] - unitSize[1] * unitVelocity[1] > 390
-      ? (unitPosition[1] = 0)
-      : unitPosition[1] - unitSize[1] * unitVelocity[1] < 0
-      ? (unitPosition[1] = 400)
-      : (unitPosition[1] -= unitSize[1] * unitVelocity[1]);
-  });
-  // draw food
-  context.fillStyle = "#FFDDD1";
-  context.fillRect(foodPosition[0], foodPosition[1], unitSize[0], unitSize[1]);
 };
