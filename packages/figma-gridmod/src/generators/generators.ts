8f39379bcbcf0a2797bbcb3d66b9186ea17f29bd
gnawx <stevanus.satria@gmail.com>
2020-03-22 15:32:06 +0800

    commenting out placeholders

--- packages/figma-gridmod/src/generators/generators.ts
+++ packages/figma-gridmod/src/generators/generators.ts
@@ -1,26 +1,26 @@
 import { SelectedGrid } from "../models/interfaces";
 
-export const editColumns = (
-  selectedGrid: SelectedGrid,
-  decrease: boolean,
-  amount: number,
-  all: boolean,
-  index?: number,
-): void => {
-  // TODO: create editBorders, editRowBackgrounds, editTexts, editTableHeader
-  return null;
-};
+// export const editColumns = (
+//   selectedGrid: SelectedGrid,
+//   decrease: boolean,
+//   amount: number,
+//   all: boolean,
+//   index?: number,
+// ): void => {
+//   // TODO: create editBorders, editRowBackgrounds, editTexts, editTableHeader
+//   return null;
+// };
 
-export const editRows = (
-  selectedGrid: SelectedGrid,
-  decrease: boolean,
-  amount: number,
-  all: boolean,
-  index?: number,
-): void => {
-  // TODO: create editBorders, editRowBackgrounds, editTexts, editTableHeader
-  return null;
-};
+// export const editRows = (
+//   selectedGrid: SelectedGrid,
+//   decrease: boolean,
+//   amount: number,
+//   all: boolean,
+//   index?: number,
+// ): void => {
+//   // TODO: create editBorders, editRowBackgrounds, editTexts, editTableHeader
+//   return null;
+// };
 
 export const getBorderTypesId = (selectedGrid: SelectedGrid): { [key: string]: string } => {
   const allBorders: { [key: string]: string } = {};
@@ -30,13 +30,13 @@ export const getBorderTypesId = (selectedGrid: SelectedGrid): { [key: string]: s
   return allBorders;
 };
 
-const editBorders = (selectedGrid: SelectedGrid, row: boolean, amount: number, all: boolean, index?: number): void => {
-  const allBorders: { [key: string]: string } = getBorderTypesId(selectedGrid);
-  row ? resizeBorders(allBorders["Vertical"], amount, all) : resizeBorders(allBorders["Horizontal"], amount, all);
-};
+// const editBorders = (selectedGrid: SelectedGrid, row: boolean, amount: number, all: boolean, index?: number): void => {
+//   const allBorders: { [key: string]: string } = getBorderTypesId(selectedGrid);
+//   row ? resizeBorders(allBorders["Vertical"], amount, all) : resizeBorders(allBorders["Horizontal"], amount, all);
+// };
 
-const resizeBorders = (id: string, amount: number, all: boolean): void => {
-  (figma.getNodeById(id) as GroupNode).children.forEach((child: LineNode) => {
-    child.resize(child.width + amount, 0); // TODO need to multiply with # of resized items and consider header presence!
-  });
-};
+// const resizeBorders = (id: string, amount: number, all: boolean): void => {
+//   (figma.getNodeById(id) as GroupNode).children.forEach((child: LineNode) => {
+//     child.resize(child.width + amount, 0); // TODO need to multiply with # of resized items and consider header presence!
+//   });
+// };
